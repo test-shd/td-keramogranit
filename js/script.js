cecaad68fbf5b81cbc240f900dbc7880a64917e8
DinaSharipova <sharipova0308@gmail.com>
2023-10-30 12:16:06 +0500

    add menu momile

--- js/script.js
+++ js/script.js
@@ -53,48 +53,48 @@ const swiper2 = new Swiper('.main-rew__slider', {
    
   });
 
-//   function animation() {
-//    gsap.registerPlugin(ScrollTrigger);
+  function animation() {
+   gsap.registerPlugin(ScrollTrigger);
 
-//     gsap.from('.main-catalog__item', {
-//       opacity: 0,
-//       yPercent: 100,
-//       stagger: 0.4,
-//       scrollTrigger: {
-//         trigger: '.main-catalog',
-//         start: '30% bottom', 
+    gsap.from('.main-catalog__item', {
+      opacity: 0,
+      yPercent: 100,
+      stagger: 0.4,
+      scrollTrigger: {
+        trigger: '.main-catalog',
+        start: '30% bottom', 
        
-//         toggleActions: 'play none none reverse',
-//       }
-//     });
+        toggleActions: 'play none none reverse',
+      }
+    });
 
-//     gsap.to('.promo__img', {
-//       'width': '170%',
-//       scrollTrigger: {
-//         start: 'top top',
-//         end: '400px',
-//         // markers: true,
-//         scrub: true,
-//       }
-//     });
+    gsap.to('.promo__img', {
+      'width': '170%',
+      scrollTrigger: {
+        start: 'top top',
+        end: '400px',
+        // markers: true,
+        scrub: true,
+      }
+    });
 
-//     const tlImg = gsap.timeline({
-//       scrollTrigger: {
-//         trigger: '.main-about__images',
-//         start: 'top top',
-//         pin: true,
-//         scrub: 1,
-//       }
-//     });
-//     tlImg.to('.main-about__img:first-child img', {
-//       scale: 1
-//     });
-//     tlImg.to('.main-about__img:last-child img', {
-//       scale: 0
-//     }, '<');
+    const tlImg = gsap.timeline({
+      scrollTrigger: {
+        trigger: '.main-about__images',
+        start: 'top top',
+        pin: true,
+        scrub: 1,
+      }
+    });
+    tlImg.to('.main-about__img:first-child img', {
+      scale: 1
+    });
+    tlImg.to('.main-about__img:last-child img', {
+      scale: 0
+    }, '<');
    
-//  }
-//  animation();
+ }
+ animation();
 
 
 
